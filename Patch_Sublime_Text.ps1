a0ecaaaa995bf28ecef7cf522bc1d6189972d894
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2025-03-23 21:37:11 -0300

    Add files via upload

--- Patch_Sublime_Text.ps1
+++ Patch_Sublime_Text.ps1
@@ -31,15 +31,15 @@ Write-Host "====================================================" -ForegroundCol
 Write-Host ""  # Espacio en blanco para mayor claridad visual
 
 # Verificar y establecer la política de ejecución para evitar restricciones
-Write-Host "[ * ] Verificando política de ejecución..." -ForegroundColor $Yellow
+Write-Host "[*] Verificando política de ejecución..." -ForegroundColor $Yellow
 $executionPolicy = Get-ExecutionPolicy
 
 if ($executionPolicy -ne "RemoteSigned") {
-    Write-Host "[ * ] Configurando política de ejecución a 'RemoteSigned'..." -ForegroundColor $Cyan
+    Write-Host "[*] Configurando política de ejecución a 'RemoteSigned'..." -ForegroundColor $Cyan
     Set-ExecutionPolicy RemoteSigned -Force
-    Write-Host "[ + ] Política de ejecución configurada correctamente." -ForegroundColor $Green
+    Write-Host "[+] Política de ejecución configurada correctamente." -ForegroundColor $Green
 } else {
-    Write-Host "[ + ] La política de ejecución ya está establecida en 'RemoteSigned'" -ForegroundColor $Green
+    Write-Host "[+] La política de ejecución ya está establecida en 'RemoteSigned'" -ForegroundColor $Green
 }
 
 # Función para descargar el archivo de configuración a una carpeta temporal
@@ -49,14 +49,14 @@ function Download-ConfigFile {
         [string]$destinationPath  # Ruta de destino donde guardar el archivo
     )
 
-    Write-Host "[ * ] Descargando archivo de configuración desde: $url" -ForegroundColor $Yellow
+    Write-Host "[*] Descargando archivo de configuración desde: $url" -ForegroundColor $Yellow
 
     try {
         # Descargar el archivo desde la URL
         Invoke-WebRequest -Uri $url -OutFile $destinationPath
-        Write-Host "[ + ] Archivo descargado correctamente a: $destinationPath" -ForegroundColor $Green
+        Write-Host "[+] Archivo descargado correctamente a: $destinationPath" -ForegroundColor $Green
     } catch {
-        Write-Host "[ - ] Error al descargar el archivo: $_" -ForegroundColor $Red
+        Write-Host "[-] Error al descargar el archivo: $_" -ForegroundColor $Red
         Exit
     }
 }
@@ -69,7 +69,7 @@ $configFileUrl = "https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/patch
 
 # Verificar si el archivo de configuración existe
 if (-not (Test-Path -Path $configFilePath)) {
-    Write-Host "[ - ] El archivo de configuración no existe localmente. Intentando descargarlo..." -ForegroundColor $Red
+    Write-Host "[-] El archivo de configuración no existe localmente. Intentando descargarlo..." -ForegroundColor $Red
     
     # Obtener una ruta temporal para guardar el archivo descargado
     $tempConfigFilePath = Join-Path -Path $env:TEMP -ChildPath "patch_config.txt"
@@ -85,7 +85,7 @@ if (-not (Test-Path -Path $configFilePath)) {
 $configLines = Get-Content -Path $configFilePath
 
 # Mostrar las últimas 5 versiones disponibles en la base de datos
-Write-Host "[ > ] Últimas 5 versiones disponibles en la base de datos:" -ForegroundColor $Cyan
+Write-Host "[>] Últimas 5 versiones disponibles en la base de datos:" -ForegroundColor $Cyan
 Write-Host ""  # Espacio en blanco para mayor claridad visual
 $versions = @()
 foreach ($line in $configLines) {
@@ -107,7 +107,7 @@ $installedVersion = Read-Host "Por favor ingrese la versión de Sublime Text (po
 
 # Validar la versión ingresada (solo números)
 if ($installedVersion -notmatch '^\d+$') {
-    Write-Host "[ - ] Versión no válida. Asegúrese de ingresar un número válido." -ForegroundColor $Red
+    Write-Host "[-] Versión no válida. Asegúrese de ingresar un número válido." -ForegroundColor $Red
     Exit
 }
 
@@ -125,7 +125,7 @@ foreach ($line in $configLines) {
         if ($versionInConfig -eq $installedVersion) {
             $patchFound = $true
             Write-Host ""  # Espacio en blanco para mayor claridad visual
-            Write-Host "[ + ] Parche encontrado para la versión $installedVersion" -ForegroundColor $Green
+            Write-Host "[+] Parche encontrado para la versión $installedVersion" -ForegroundColor $Green
             Write-Host "FindBytes: $findBytes"
             Write-Host "ReplaceBytes: $replaceBytes"
             break
@@ -136,7 +136,7 @@ foreach ($line in $configLines) {
 # Si no encontramos el parche, salimos
 if (-not $patchFound) {
     Write-Host ""  # Espacio en blanco para mayor claridad visual
-    Write-Host "[ - ] No se encontró el parche para la versión $installedVersion" -ForegroundColor $Red
+    Write-Host "[-] No se encontró el parche para la versión $installedVersion" -ForegroundColor $Red
     Exit
 }
 
@@ -152,7 +152,7 @@ if ($modifyHosts -eq '') {
 }
 
 if ($modifyHosts -eq 'S' -or $modifyHosts -eq 's') {
-    Write-Host "[ * ] Modificando archivo hosts..." -ForegroundColor $Yellow
+    Write-Host "[*] Modificando archivo hosts..." -ForegroundColor $Yellow
     
     $hostsFile = "C:\Windows\System32\drivers\etc\hosts"
 
@@ -160,14 +160,14 @@ if ($modifyHosts -eq 'S' -or $modifyHosts -eq 's') {
     if (Test-Path -Path $hostsFile) {
         # Intentar dar permisos de escritura al archivo hosts
         try {
-            Write-Host "[ * ] Intentando cambiar permisos del archivo hosts..." -ForegroundColor $Yellow
+            Write-Host "[*] Intentando cambiar permisos del archivo hosts..." -ForegroundColor $Yellow
             $acl = Get-Acl $hostsFile
             $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "Write", "Allow")
             $acl.AddAccessRule($accessRule)
             Set-Acl -Path $hostsFile -AclObject $acl
-            Write-Host "[ + ] Permisos cambiados para permitir escritura." -ForegroundColor $Green
+            Write-Host "[+] Permisos cambiados para permitir escritura." -ForegroundColor $Green
         } catch {
-            Write-Host "[ - ] No se pudieron cambiar los permisos del archivo hosts." -ForegroundColor $Red
+            Write-Host "[-] No se pudieron cambiar los permisos del archivo hosts." -ForegroundColor $Red
         }
 
         $existingHosts = Get-Content -Path $hostsFile
@@ -184,11 +184,11 @@ if ($modifyHosts -eq 'S' -or $modifyHosts -eq 's') {
         foreach ($entry in $entries) {
             if ($existingHosts -notcontains $entry) {
                 Add-Content -Path $hostsFile -Value $entry
-                Write-Host "[ + ] Entrada agregada: $entry" -ForegroundColor $Green
+                Write-Host "[+] Entrada agregada: $entry" -ForegroundColor $Green
             }
         }
     } else {
-        Write-Host "[ - ] El archivo hosts no se encuentra en la ubicación esperada." -ForegroundColor $Red
+        Write-Host "[-] El archivo hosts no se encuentra en la ubicación esperada." -ForegroundColor $Red
     }
 }
 
@@ -208,20 +208,20 @@ function Get-FileHashValue {
 }
 
 # Verificar el hash antes del parche
-Write-Host "[ * ] Calculando el hash del archivo original..." -ForegroundColor $Yellow
+Write-Host "[*] Calculando el hash del archivo original..." -ForegroundColor $Yellow
 $originalHash = Get-FileHashValue -filePath $filePath
-Write-Host "[ > ] Hash original: $originalHash" -ForegroundColor $Cyan
+Write-Host "[>] Hash original: $originalHash" -ForegroundColor $Cyan
 
 # Confirmar si el archivo está en uso
-Write-Host "[ * ] Esperando que el archivo sublime_text.exe no esté en uso..." -ForegroundColor $Yellow
+Write-Host "[*] Esperando que el archivo sublime_text.exe no esté en uso..." -ForegroundColor $Yellow
 Start-Sleep -Seconds 3
 
 # Intentar modificar el archivo sublime_text.exe
 try {
-    Write-Host "[ * ] Leyendo el archivo sublime_text.exe..." -ForegroundColor $Yellow
+    Write-Host "[*] Leyendo el archivo sublime_text.exe..." -ForegroundColor $Yellow
     $fileBytes = [System.IO.File]::ReadAllBytes($filePath)
 
-    Write-Host "[ * ] Reemplazando bytes en el archivo sublime_text.exe..." -ForegroundColor $Yellow
+    Write-Host "[*] Reemplazando bytes en el archivo sublime_text.exe..." -ForegroundColor $Yellow
     $findBytesArray = $findBytes -split ' '
     $replaceBytesArray = $replaceBytes -split ' '
 
@@ -249,29 +249,29 @@ try {
         }
     }
 
-    Write-Host "[ * ] Guardando el archivo modificado..." -ForegroundColor $Yellow
+    Write-Host "[*] Guardando el archivo modificado..." -ForegroundColor $Yellow
     [System.IO.File]::WriteAllBytes($filePath, $fileBytes)
-    Write-Host "[ + ] Proceso completado exitosamente." -ForegroundColor $Green
+    Write-Host "[+] Proceso completado exitosamente." -ForegroundColor $Green
 } catch {
-    Write-Host "[ - ] Hubo un error al modificar el archivo: $_" -ForegroundColor $Red
+    Write-Host "[-] Hubo un error al modificar el archivo: $_" -ForegroundColor $Red
 }
 
 # Verificar si el parche fue exitoso comparando el hash después del parche
-Write-Host "[ * ] Calculando el hash del archivo modificado..." -ForegroundColor $Yellow
+Write-Host "[*] Calculando el hash del archivo modificado..." -ForegroundColor $Yellow
 $modifiedHash = Get-FileHashValue -filePath $filePath
-Write-Host "[ > ] Hash modificado: $modifiedHash" -ForegroundColor $Cyan
+Write-Host "[>] Hash modificado: $modifiedHash" -ForegroundColor $Cyan
 
 # Comparar los hashes
 if ($originalHash -eq $modifiedHash) {
-    Write-Host "[ - ] El parche NO fue exitoso. El archivo no ha cambiado." -ForegroundColor $Red
+    Write-Host "[-] El parche NO fue exitoso. El archivo no ha cambiado." -ForegroundColor $Red
 } else {
-    Write-Host "[ + ] El parche fue exitoso. El archivo ha sido modificado." -ForegroundColor $Green
+    Write-Host "[+] El parche fue exitoso. El archivo ha sido modificado." -ForegroundColor $Green
 }
 
-Write-Host "[ * ] Restaurando política de ejecución a 'Restricted'..." -ForegroundColor $Yellow
+Write-Host "[*] Restaurando política de ejecución a 'Restricted'..." -ForegroundColor $Yellow
 Set-ExecutionPolicy Restricted -Force
-Write-Host "[ + ] Política de ejecución actual: $(Get-ExecutionPolicy)" -ForegroundColor $Green
+Write-Host "[+] Política de ejecución actual: $(Get-ExecutionPolicy)" -ForegroundColor $Green
 
 # Evitar que la ventana de PowerShell se cierre automáticamente
-Write-Host "[ * ] La ventana no se cerrará automáticamente. Puedes cerrarla manualmente." -ForegroundColor $Yellow
+Write-Host "[*] La ventana no se cerrará automáticamente. Puedes cerrarla manualmente." -ForegroundColor $Yellow
 Write-Host ""  # Espacio en blanco para mayor claridad visual
